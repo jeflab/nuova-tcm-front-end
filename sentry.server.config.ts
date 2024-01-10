ac4e3ecdfc979df05829d94bec42ef05f811f3ac
Fabio <lazzaroni@jeflab.it>
2024-01-10 13:19:28 +0100

    🥅: Installato sentry per la gestione degli errori.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cf295f6b6cf51e267fdfd84c56895dcf@o66710.ingest.sentry.io/4506546631081984",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ src/app/error.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import styles from "@/app/(no-menu)/layout.module.scss";
+import {ButtonLink} from "@/ui/ButtonLink";
+import CenterLogoContent from "@/ui/CenterLogoContent";
+import {Debug} from "@/ui/Debug";
+import {
+  faArrowRotateBack,
+  faHouseChimney,
+} from "@fortawesome/pro-duotone-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import * as Sentry from "@sentry/nextjs";
+import {useEffect} from "react";
+import {Button} from "react-bootstrap";
+
+const containerStyle = {"--content-width": "max-content"};
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & {digest?: string};
+  reset: () => void;
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <main className={styles.publicMain}>
+      <CenterLogoContent style={containerStyle}>
+        <h2>Qualcosa è andato storto 😕</h2>
+        <Button onClick={() => reset()} type="button" className="w-100">
+          <FontAwesomeIcon icon={faArrowRotateBack} className="me-2" />
+          Riprova
+        </Button>
+        <ButtonLink href="/" type="button" className="w-100">
+          <FontAwesomeIcon icon={faHouseChimney} className="me-2" />
+          Torna alla pagina principale
+        </ButtonLink>
+        {process.env.NODE_ENV === "development" && (
+          <>
+            <h3>{error.name}</h3>
+            <p>{error.message}</p>
+            <Debug>{error}</Debug>
+          </>
+        )}
+      </CenterLogoContent>
+    </main>
+  );
+}
