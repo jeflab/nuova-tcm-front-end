6175e1ea4af704574f501060881a37dea7ef641a
Fabio <lazzaroni@jeflab.it>
2024-01-14 16:18:48 +0100

    💄(quoter): Barra preventivatore a tutto schermo

--- src/app/(menu)/quoter/page.tsx
+++ src/app/(menu)/quoter/page.tsx
@@ -1,19 +1,21 @@
 import {AppContainer} from "@/ui/AppContainer";
 import {PageTitle} from "@/ui/PageTitle";
-import {Alert} from "react-bootstrap";
+import {Alert, Stack} from "react-bootstrap";
 import {QuoterForm} from "./QuoterForm";
 
 export default function Quoter() {
   return (
-    <AppContainer className="vstack gap-3">
-      <PageTitle>Preventivatore</PageTitle>
-      <Alert variant="info" className="mb-0">
-        Si prega di compilare il form per il preventivo della polizza vita con
-        attenzione alle normative vigenti e principi etici. Concentrarsi sul
-        benessere del cliente, garantendo chiarezza, trasparenza e riservatezza
-        nelle informazioni è fondamentale.
-      </Alert>
+    <Stack gap={3}>
+      <AppContainer>
+        <PageTitle>Preventivatore</PageTitle>
+        <Alert variant="info" className="mb-0">
+          Si prega di compilare il form per il preventivo della polizza vita con
+          attenzione alle normative vigenti e principi etici. Concentrarsi sul
+          benessere del cliente, garantendo chiarezza, trasparenza e
+          riservatezza nelle informazioni è fondamentale.
+        </Alert>
+      </AppContainer>
       <QuoterForm />
-    </AppContainer>
+    </Stack>
   );
 }
