354e99d864da55b9a8f4fc7172027c5312de5efa
Fabio <lazzaroni@jeflab.it>
2024-01-24 21:46:48 +0100

    🏷️: Utility type per migliorare la leggibilità dei tipi

--- src/ui/types.ts
+++ src/ui/types.ts
@@ -3,3 +3,7 @@ import {ReactNode} from "react";
 export interface WithChildren {
   children?: ReactNode;
 }
+
+type Identity<T> = T extends object ? {[K in keyof T]: T[K]} : T;
+
+type Prettify<T> = {[K in keyof T]: T[K]} & {};
