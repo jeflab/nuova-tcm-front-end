fc3412ea01006b1d921963423bba1b268c438b6c
Fabio <lazzaroni@jeflab.it>
2024-02-06 14:45:41 +0100

    🔥(newLip): Rimossi dati di esempio dalla produzione

--- src/app/(menu)/(authenticated)/lips/[id]/page.tsx
+++ src/app/(menu)/(authenticated)/lips/[id]/page.tsx
@@ -1,4 +1,3 @@
-import {DebugState} from "@/app/(menu)/(authenticated)/lips/[id]/DebugState";
 import {drawers} from "@/app/(menu)/(authenticated)/lips/[id]/drawers";
 import {Lip} from "@/app/(menu)/(authenticated)/lips/model";
 import {cns} from "@/helpers/cns";
@@ -54,7 +53,7 @@ export default async function NewLipPage() {
               corrispondere.
             </p>
           </Alert>
-          <DebugState />
+          {/*<DebugState />*/}
           {drawers.map(({name, title, modalContent, summaryContent}) => (
             <Drawer
               key={name}
