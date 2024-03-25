bb2a7e6b2e06ec4d57a7a83da8de3d9e42d4a59d
Fabio <lazzaroni@jeflab.it>
2024-03-25 16:43:23 +0100

    ✏️(newLip): Sistemato typo "il contraente dichiara" -> "L'intermediario dichiara"

--- src/app/(menu)/(authenticated)/lipsDrawers/IdentificationForm.tsx
+++ src/app/(menu)/(authenticated)/lipsDrawers/IdentificationForm.tsx
@@ -223,7 +223,7 @@ export function IdentificationForm() {
                 </DropzoneField>
               </FormGroup>
             </Col>{" "}
-            <h4>Il contraente dichiara:</h4>
+            <h4>L'intermediario dichiara:</h4>
             <Col xs={12}>
               <FormGroup
                 controlId="metContractorInPerson"
