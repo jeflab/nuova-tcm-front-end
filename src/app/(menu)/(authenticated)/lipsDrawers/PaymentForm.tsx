ca2b20970b2f8044e34dd70348fe89dcef3c5963
Fabio <lazzaroni@jeflab.it>
2024-04-03 21:42:56 +0200

    🐛(lips): Risolto errore paymentMethod vuoto

--- src/app/(menu)/(authenticated)/lipsDrawers/PaymentForm.tsx
+++ src/app/(menu)/(authenticated)/lipsDrawers/PaymentForm.tsx
@@ -197,6 +197,9 @@ export function PaymentForm() {
                 <CheckGroup
                   type="radio-switch"
                   options={paymentMethodsOptions(premium, paymentMethodValue)}
+                  validation={{
+                    required: "Seleziona un frazionamento di pagamento",
+                  }}
                 />
               </FormGroup>
             </Col>
