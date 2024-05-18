ad9e062cabedaaa009ad6c9fc39cef11a71ccb9e
Fabio <lazzaroni@jeflab.it>
2024-05-18 21:05:51 +0200

    💄: Nuova icona per underwriting sanitario

--- src/models/entities/lip.tsx
+++ src/models/entities/lip.tsx
@@ -16,8 +16,8 @@ import {
 } from "@fortawesome/pro-duotone-svg-icons";
 import {
   faCircle,
+  faHeartPulse,
   faSackDollar,
-  faUserMd,
 } from "@fortawesome/pro-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {ReactNode} from "react";
@@ -30,7 +30,7 @@ export const LipStatesIcons: Record<number, ReactNode> = {
   2: (
     <IconStack className="text-danger">
       <FontAwesomeIcon icon={faCircle} className="fa-stack-2x" opacity={0.4} />
-      <FontAwesomeIcon icon={faUserMd} className="fa-stack-1x" />
+      <FontAwesomeIcon icon={faHeartPulse} className="fa-stack-1x" />
     </IconStack>
   ),
   3: <FontAwesomeIcon icon={faCheckCircle} className="text-success" />,
