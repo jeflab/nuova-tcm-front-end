bedc7a60d295835999f182607256f7ff58a28900
Fabio Lazzaroni <lazzaroni@jeflab.it>
2025-05-27 15:22:50 +0200

    🔧: Messo react compiler dietro una variabile d'ambiente

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,16 @@ import process from "node:process";
 
 process.env.SENTRY_RELEASE = getRelease();
 console.info(`Sentry release: ${process.env.SENTRY_RELEASE}`);
+
 const maintenanceMode = process.env.MAINTENANCE_MODE === "true";
 console.info(`Maintenance mode: ${maintenanceMode}`);
 
+const enableReactCompiler = process.env.ENABLE_TRACT_COMPILER === "true";
+console.info(`React compiler enabled: ${enableReactCompiler}`);
+
 let nextConfig: NextConfig = {
   experimental: {
-    // React compiler pronto
-    //   reactCompiler: true,
+    reactCompiler: enableReactCompiler,
     serverActions: {
       bodySizeLimit: "16mb",
     },
