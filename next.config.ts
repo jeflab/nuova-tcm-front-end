9fad0cdd3967c75d98a60e9b9c3dcea4ff29c9ae
Fabio Lazzaroni <lazzaroni@jeflab.it>
2025-07-16 16:08:57 +0200

    🚚: Rinominata route per sentry in modo da bypassare ublock

--- next.config.ts
+++ next.config.ts
@@ -73,7 +73,7 @@ nextConfig = withSentryConfig(nextConfig, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  tunnelRoute: "/health-check",
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
